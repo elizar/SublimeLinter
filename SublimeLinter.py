9ace64ad610037eb289305f1e582fb2afeacf945
Elizar Pepino <jupenz@gmail.com>
2013-08-25 14:13:27 +0800

    Fixed gutter image path issues

--- SublimeLinter.py
+++ SublimeLinter.py
@@ -39,7 +39,7 @@ DELAYS = (
 # "alpha", "bright", "dark", "hard" and "simple"
 MARK_THEMES = ('alpha', 'bright', 'dark', 'hard', 'simple')
 # The path to the built-in gutter mark themes
-MARK_THEMES_PATH = os.path.join('..', 'SublimeLinter', 'gutter_mark_themes')
+MARK_THEMES_PATH = os.path.join('Packages', 'SublimeLinter', 'gutter_mark_themes')
 # The original theme for anyone interested the previous minimalist approach
 ORIGINAL_MARK_THEME = {
     'violation': 'dot',
@@ -258,7 +258,7 @@ def add_lint_marks(view, lines, error_underlines, violation_underlines, warning_
                     if gutter_mark_theme == 'original':
                         gutter_mark_image = ORIGINAL_MARK_THEME[lint_type]
                     elif gutter_mark_theme in MARK_THEMES:
-                        gutter_mark_image = os.path.join(MARK_THEMES_PATH, gutter_mark_theme + '-' + lint_type)
+                        gutter_mark_image = os.path.join(MARK_THEMES_PATH, gutter_mark_theme + '-' + lint_type + '.png')
                     else:
                         gutter_mark_image = gutter_mark_theme + '-' + lint_type
 
